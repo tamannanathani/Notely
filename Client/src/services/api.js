0e27db5443db4f3078feb606accfd0a5cf16290f
Tamanna Nathani <tamannanathani45@gmail.com>
2025-09-04 15:09:01 +0530

    feat: Update index.html title and change API base URL to production

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://notely-production-a4ee.up.railway.app/",
 });
 
 api.interceptors.request.use((config) => {
